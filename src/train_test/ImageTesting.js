0a572bb399ec938049942394d0d98ba584c3a8af
Drew Devero <deverobelfon@gmail.com>
2021-01-20 05:08:18 -0500

    added images

--- src/train_test/ImageTesting.js
+++ src/train_test/ImageTesting.js
@@ -1,27 +1,44 @@
 const fs = require("fs");
+const { promisify } = require("util");
+const withNodeSleep = promisify(setTimeout);
+/* const inBrowserSleep = (milliseconds) => {
+    return new Promise(res => setTimeout(res, milliseconds))
+} */
 
 const labels = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const numOfImages = 21;
 let totalImages = 0;
 let imageCapture;
+// const captureButton = document.querySelector("button");
 
-(function getUserMedia() {
-    navigator.mediaDevices.getUserMedia({video: true})
-     .then(mediaStream => {
+let mediaStream = null;
+/* (async function getUserMedia() {
+    try {
+        mediaStream = await navigator.mediaDevices.getUserMedia({video: { width:1280, height:720 }})
         document.querySelector("video").srcObject = mediaStream;
-
-     let track = mediaStream.getVideoTracks()[0];
-     let imageCapture = new ImageCapture(track)
-    })
-     .catch(error => console.log(error));
-})();
-
-labels.forEach((char) => {
-    fs.mkdir(`../images/${char}`, () => null);
-    for(let i = 1; i < numOfImages; i++) {
-        fs.writeFile(`../images/${char}/label_${char}_${i.toString()}.png`, "utf8", () => null )
-    totalImages++;
+    } catch(error) {
+        console.log(error)
     }
-    return totalImages;
-})
-console.log(totalImages)
+})(); */
+
+// captureButton.onclick = takeScreenShots;
+
+function takeScreenShots() {
+    const track = mediaStream.getVideoTracks()[0];
+    imageCapture = new ImageCapture(track);
+    saveImages();
+}
+
+ function saveImages() {
+    labels.forEach((char) => {
+        withNodeSleep(1000).then(() => fs.mkdir(`../images/${char}`, () => null),
+        (async function populateDirs() {
+            for(let i = 1; i < numOfImages; i++) {
+                await withNodeSleep(1000).then(() => {fs.writeFile(`../images/${char}/label_${char}_${i.toString()}.png`, /* imageCapture.grabFrame() */"utf8", () => null);
+                })
+            }
+        })(),
+        /* populateDirs() */
+    )})
+}
+saveImages();
