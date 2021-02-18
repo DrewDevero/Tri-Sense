56a7ebc6fb2e1752d6f009f3596a23dd9409cf38
Drew Devero <deverobelfon@gmail.com>
2021-02-18 01:22:01 -0500

    added site explaination and styling

--- client/src/Home.js
+++ client/src/Home.js
@@ -13,6 +13,14 @@ return(
                 <div className="basic-margin center-text">
                 <a href={ ASL_Alphabet } target="_blank" rel="noreferrer" alt="American Sign Language Alphabet">ASL Alphabet</a>
                 </div>
+                <section className="center">
+                    <article className="mission-background">
+                        <p className="mission-heading-first-word">A</p>
+                        <p className="mission-heading-second-word">Mission</p>
+                        <p className="mission-paragraph-1"><span className="work-in-progress">A work in progress:</span> Dictionary look up app with returning word, pronunciation(written form), part of speech, and vocal file giving auditory example of pronunciation. Accessibility level considered adequate as determined by Axe Accessibility.</p>
+                        <p></p>
+                    </article>
+                </section>
             </div>
         </main>
     )
