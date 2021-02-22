30bfd02fa0442a059151974f2b94f379955ddd3d
Drew Devero <deverobelfon@gmail.com>
2021-02-22 15:05:57 -0500

    refactored file and modified config and props

--- server/src/main/java/com/example/server/config/WebConfig.java
+++ server/src/main/java/com/example/server/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class Configurer implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer{
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
